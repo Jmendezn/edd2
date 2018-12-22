c7a0a17fd24d4e3313626ece6dd7e0ba419f8b0f
Jmendezn <jhmn_@hotmail.com>
2018-12-22 13:25:15 -0700

    22-12

---
+++ edd2/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace edd2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(480, 148);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 20);
+            this.textBox5.TabIndex = 42;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(480, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 20);
+            this.textBox4.TabIndex = 41;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(480, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 20);
+            this.textBox3.TabIndex = 40;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Nodos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Total:";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(585, 219);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 19);
+            this.button8.TabIndex = 37;
+            this.button8.Text = "Recorrido";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(497, 219);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 19);
+            this.button7.TabIndex = 36;
+            this.button7.Text = "Contraer";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(585, 173);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 19);
+            this.button6.TabIndex = 35;
+            this.button6.Text = "Eliminar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(404, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 19);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "Expandir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(585, 148);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 19);
+            this.button4.TabIndex = 33;
+            this.button4.Text = "Agregar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(585, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 19);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Agregar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(585, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 19);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(585, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 19);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(480, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 20);
+            this.textBox2.TabIndex = 29;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(480, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 28;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(480, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(401, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Hijo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Padre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Generaciones";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Raiz:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(392, 365);
+            this.treeView1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 380);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ edd2/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace edd2
+{
+    public partial class Form1 : Form
+    {
+
+
+        string recorrido = "";
+        int tt = 0;
+        int x = 1;
+        int ConG = 0;
+        int C1 = 0;
+        int A1 = 0;
+        int G1 = 2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            C1 = 0;
+
+            int GenTT = Convert.ToInt32(textBox1.Text);
+            treeView1.Nodes.Add("Jesus Mendez");
+            button3.Visible = false;
+           
+
+            C1++;
+            if (GenTT > 0 && GenTT < 11)
+            {
+                Genod(treeView1.Nodes[0], A1, GenTT);
+            }
+            else
+            {
+                MessageBox.Show("no cumple con los requisitos");
+            }
+
+            textBox3.Text = C1.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            {
+                MessageBox.Show("" + treeView1.SelectedNode.Text);
+                comboBox1.Text = treeView1.SelectedNode.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int CalCG = Convert.ToInt32(textBox1.Text);
+
+            for (int i = 0; i < CalCG; i++)
+            {
+                tt = x * 2;
+                x = x * 2;
+                ConG = ConG + tt;
+            }
+            textBox2.Text = ConG.ToString();
+        }
+
+        public void Genod(TreeNode node, int A11, int GenT)
+        {
+            C1++;
+            A11++;
+
+            node.Nodes.Add("Padre " + G1.ToString());
+            comboBox1.Items.Add("Padre" + G1.ToString());
+            recorrido += node.Text + ",";
+            G1++;
+            if (A11 < GenT)
+            {
+                Genod(node.Nodes[0], A11, GenT);
+            }
+
+            node.Nodes.Add("Madre " + G1.ToString());
+            comboBox1.Items.Add("Padre " + G1.ToString());
+            recorrido += node.Text + ",";
+            G1++;
+            if (A11 < GenT)
+            {
+                Genod(node.Nodes[1], A11, GenT);
+            }
+
+            return;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(textBox4.Text);
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode != null)
+            {
+                string hijo;
+                hijo = textBox5.Text;
+                treeView1.SelectedNode.Nodes.Add(hijo);
+                treeView1.ExpandAll();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(recorrido);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            treeView1.CollapseAll();
+        }
+    }
+}
